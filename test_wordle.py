bbf04f4098562064f40b5e2d9be5e2a06d04e87e
Vill Ain <vill99green@gmail.com>
2024-06-04 07:41:40 -1000

    Use a fixture

--- test_wordle.py
+++ test_wordle.py
@@ -4,49 +4,52 @@ from wordle import Letter
 from wordle import Wordle
 from wordle import Word
 
-
-def test_set_answer():
+@pytest.fixture
+def mywordle():
     mywordle = Wordle()
+    return mywordle
+
+
+def test_set_answer(mywordle):
     mywordle.set_answer("testy")
     assert mywordle.answer == "testy"
 
 
-def test_first_pass():
-    mywordle = Wordle("sweet")
+def test_first_pass(mywordle):
+    mywordle.set_answer("sweet")
     output, remainder = mywordle.first_pass("tweet")
     assert remainder == ["s"]
     assert output == ["", Letter("w"), Letter("e"), Letter("e"), Letter("t")]
 
 
-def test_first_pass_2():
-    mywordle = Wordle("eweet")
+def test_first_pass_2(mywordle):
+    mywordle.set_answer("eweet")
     output, remainder = mywordle.first_pass("tweet")
     assert remainder == ["e"]
     assert output == ["", Letter("w"), Letter("e"), Letter("e"), Letter("t")]
 
 
-def test_first_pass_3():
-    mywordle = Wordle("sweat")
+def test_first_pass_3(mywordle):
+    mywordle.set_answer("sweat")
     output, remainder = mywordle.first_pass("tweet")
     assert remainder == ["s", "a"]
     assert output == ["", Letter("w"), Letter("e"), "", Letter("t")]
 
 
-def test_first_pass_miss():
-    mywordle = Wordle("sweat")
+def test_first_pass_miss(mywordle):
+    mywordle.set_answer("sweat")
     output, remainder = mywordle.first_pass("mound")
     assert remainder == ["s", "w", "e", "a", "t"]
     assert output == ["", "", "", "", ""]
 
 
-def test_output_letter_list():
-    mywordle = Wordle()
+def test_output_letter_list(mywordle):
     mylist = mywordle.output_letter_list(Word(a_string="pound"))
     assert mylist == ["p", "o", "u", "n", "d"]
 
 
-def test_second_pass():
-    mywordle = Wordle("trees")
+def test_second_pass(mywordle):
+    mywordle.set_answer("trees")
     output, remainder = mywordle.first_pass("sweet")
     assert remainder == ["t", "r", "s"]
     assert output == ["", "", "e", "e", ""]
